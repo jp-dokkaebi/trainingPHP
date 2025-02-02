1b1fb42688ac8faade698d2eedcae64e22e5ef40
Jeff Nitro <nares.jeffnitro@gmail.com>
2025-02-02 23:00:52 +0900

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,3 +1,44 @@
-<html>
-    
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Title -->
+		<title>Unify - Responsive Website Template</title>
+
+		<!-- Required Meta Tags Always Come First -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="favicon.ico">
+
+		<!-- CSS Global Compulsory -->
+		<link rel="stylesheet" href="assets/vendor/bootstrap/bootstrap.min.css">
+
+		<!-- CSS Unify -->
+		<link rel="stylesheet" href="assets/css/unify-core.css">
+		<link rel="stylesheet" href="assets/css/unify-components.css">
+		<link rel="stylesheet" href="assets/css/unify-globals.css">
+
+		<!-- CSS Customization -->
+		<link rel="stylesheet" href="assets/css/custom.css">
+	</head>
+
+	<body>
+	  <main>
+			<h1>Hello, World!</h1>
+	  </main>
+
+	  <!-- JS Global Compulsory -->
+	  <script src="assets/vendor/jquery/jquery.min.js"></script>
+	  <script src="assets/vendor/jquery-migrate/jquery-migrate.min.js"></script>
+	  <script src="assets/vendor/popper.min.js"></script>
+	  <script src="assets/vendor/bootstrap/bootstrap.min.js"></script>
+
+	  <!-- JS Unify -->
+	  <script src="assets/js/hs.core.js"></script>
+
+	  <!-- JS Custom -->
+	  <script src="assets/js/custom.js"></script>
+	</body>
 </html>
